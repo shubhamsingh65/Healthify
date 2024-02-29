718852c8e18c9064ec9e1887c2c47b6df961599c
shubhamsingh65 <shubhamyogendra.singh@gmail.com>
2024-02-29 10:16:55 +0530

    first commit

---
+++ hms/admin/include/footer.php
@@ -0,0 +1,10 @@
+<footer>
+				<div class="footer-middle">
+					<div class="pull-middle">
+					<center><span class="text-bold text-uppercase">© Healthify</span>
+					</div>
+					<div class="pull-middle">
+						<span class="go-top"><i class="ti-angle-up"></i></span>
+					</div>
+				</div>
+			</footer>
